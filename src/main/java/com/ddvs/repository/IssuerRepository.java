f701a85777d5d884fd39cfac4fda7cfd22bb192a
Your Name <you@example.com>
2026-03-08 20:00:27 +0300

    JWT authentication and security config

---
+++ src/main/java/com/ddvs/repository/IssuerRepository.java
@@ -0,0 +1,7 @@
+package com.ddvs.repository;
+
+import com.ddvs.entity.Issuer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssuerRepository extends JpaRepository<Issuer, Long> {
+}
---
+++ src/main/java/com/ddvs/security/UserDetailsImpl.java
@@ -0,0 +1,32 @@
+package com.ddvs.security;
+
+import com.ddvs.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() { return user.getPassword(); }
+
+    @Override
+    public String getUsername() { return user.getEmail(); }
+
+    public Long getId() { return user.getId(); }
+
+    public String getRole() { return user.getRole().name(); }
+
+
+}
