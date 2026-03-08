a99b5026baf104e113954d86d3920b97fe82fa90
Your Name <you@example.com>
2026-03-08 20:08:12 +0300

    issuer management CRUD

---
+++ src/main/java/com/ddvs/dto/request/IssuerRequest.java
@@ -0,0 +1,11 @@
+package com.ddvs.dto.request;
+
+import lombok.Data;
+
+@Data
+public class IssuerRequest {
+
+    private String name;
+    private String organizationType;
+    private String contactEmail;
+}
